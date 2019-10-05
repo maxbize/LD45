7eec985beab7b6325ad28db786635677285eada4
Frenchie14 <mbize2005@gmail.com>
2019-10-04 19:42:34 -0700

    Debug part placement

---
+++ Assets/Scripts/ShipBuilderManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBuilderManager : MonoBehaviour
+{
+    // Set in editor
+    public Transform debugShipPartPrefab;
+    public int maxWidth;
+    public int maxHeight;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            HandleClick();
+        }
+    }
+
+    private void HandleClick() {
+        Vector3 gridPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gridPos = new Vector2(Mathf.Round(gridPos.x), Mathf.Round(gridPos.y));
+        Instantiate(debugShipPartPrefab, gridPos, Quaternion.identity);
+    }
+}
