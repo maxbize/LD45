736e0a250cd77c9648c549b71cf5dc76ec6a0bdb
Frenchie14 <mbize2005@gmail.com>
2019-10-06 00:41:40 -0700

    Simple script to keep particles with ship

---
+++ Assets/Scripts/BackgroundParticles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParticles : MonoBehaviour
+{
+    private HumanShipInput target;
+    private float nextCheck;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (target == null && Time.timeSinceLevelLoad > nextCheck) {
+            target = FindObjectOfType<HumanShipInput>();
+            nextCheck = Time.timeSinceLevelLoad + 0.5f;
+        }
+
+        if (target != null) {
+            transform.position = target.transform.position;
+        }
+    }
+}
